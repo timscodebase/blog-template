d16597aa09c37113315dbd252be9e11adae5e533
Tim Smith <tim.smith.hdg@gmail.com>
2025-09-18 22:02:32 -0700

    Update package dependencies to include @deno/svelte-adapter and adjust svelte.config.js to use the new adapter. Update pnpm-lock.yaml to reflect changes in dependencies.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@deno/svelte-adapter';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
