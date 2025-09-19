6508a57a468f9cfd6c07f6c91f1ed4eed2536684
Tim Smith <tim.smith.hdg@gmail.com>
2025-09-18 21:38:34 -0700

    first commit

---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Footer from './footer.svelte'
+	import Header from './header.svelte'
+
+	let { children } = $props()
+</script>
+
+<div class="layout">
+	<Header />
+
+	<main>
+		{@render children?.()}
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+	.layout {
+		height: 100%;
+		max-inline-size: 1440px;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		margin-inline: auto;
+		padding-inline: var(--size-7);
+
+		@media (min-width: 1440px) {
+			padding-inline: 0;
+		}
+
+		main {
+			padding-block: var(--size-9);
+		}
+	}
+</style>
